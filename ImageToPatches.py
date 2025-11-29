d2307078f21950363d717caf656547c80b74aa9f
Iran Cruz Soares <icruz2403rjso2003@gmail.com>
2025-11-28 21:01:47 -0300

    BugFixingImageToPatches

--- ImageToPatches.py
+++ ImageToPatches.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-from numpy.lib.stride_tricks import sliding_window_view
+import jax.numpy as jnp
 
 class ImageToPatches:
 
@@ -13,31 +13,37 @@ class ImageToPatches:
 
         self.patch_size = patch_size
 
-        self.H_n_patches = np.ceil(self.H/patch_size).astype(int)
+        self.H_n_patches = jnp.ceil(self.H/patch_size).astype(int)
 
-        self.W_n_patches = np.ceil(self.W/patch_size).astype(int)
+        self.W_n_patches = jnp.ceil(self.W/patch_size).astype(int)
 
-        self.pixel = np.zeros(shape = (self.H*self.W, 2))
+        self.H_pad = self.H_n_patches*self.patch_size
 
-        self.color = np.zeros(shape = (self.H*self.W, 3))
+        self.W_pad = self.W_n_patches*self.patch_size
 
-        self.pixel_patches = np.zeros(shape = (self.H_n_patches*self.patch_size, self.W_n_patches*self.patch_size, 2))
+        self.pixel = jnp.zeros(shape = (self.H*self.W, 2))
 
-        self.color_patches = np.zeros(shape = (self.H_n_patches*self.patch_size, self.W_n_patches*self.patch_size, 3))
+        self.color = jnp.zeros(shape = (self.H*self.W, 3))
+
+        self.pixel_patches = jnp.zeros(shape = (self.H_n_patches, self.W_n_patches, self.patch_size*self.patch_size, 2))
+
+        self.color_patches = jnp.zeros(shape = (self.H_n_patches, self.W_n_patches, self.patch_size*self.patch_size, 3))
 
     def image_to_pixel(self) -> None:
 
-        self.pixel = np.indices((self.H, self.W)).reshape((2, self.H*self.W)).T
+        self.pixel = jnp.indices((self.H, self.W)).reshape((2, self.H*self.W)).T
 
         self.pixel = self.pixel/self.pixel.max(axis = 0)
 
     def pixel_to_patches(self) -> None:
 
-        self.pixel_patches[ : self.H, : self.W] = self.pixel.reshape(self.H, self.W, 2)
+        padding = jnp.zeros(shape = (self.H_pad, self.W_pad, 2))
 
-        self.pixel_patches = sliding_window_view(self.pixel_patches, (self.patch_size, self.patch_size, 2))
+        padding = padding.at[ : self.H, : self.W].set(self.pixel.reshape(self.H, self.W, 2))
 
-        self.pixel_patches = self.pixel_patches[ : : self.patch_size, : : self.patch_size, 0, 0]
+        padding = padding.reshape((self.H_pad*self.W_pad, 2))
+
+        self.pixel_patches = padding.reshape((self.H_n_patches, self.W_n_patches, self.patch_size*self.patch_size, 2))
 
     def image_to_color(self) -> None:
 
@@ -47,11 +53,13 @@ class ImageToPatches:
 
     def color_to_patches(self) -> None:
 
-        self.color_patches[ : self.H, : self.W] = self.color.reshape(self.H, self.W, 3)
+        padding = jnp.zeros(shape = (self.H_pad, self.W_pad, 3))
 
-        self.color_patches = sliding_window_view(self.color_patches, (self.patch_size, self.patch_size, 3))
+        padding = padding.at[ : self.H, : self.W].set(self.color.reshape(self.H, self.W, 3))
 
-        self.color_patches = self.color_patches[ : : self.patch_size, : : self.patch_size, 0, 0]
+        padding = padding.reshape((self.H_pad*self.W_pad, 3))
+
+        self.color_patches = padding.reshape((self.H_n_patches, self.W_n_patches, self.patch_size*self.patch_size, 3))
 
     def image_to_patches(self) -> None:
 
